26717784f52d982b48648db4da6cf30de0911d28
PP6464 <panthhp@outlook.com>
2023-11-19 09:10:51 +0000

    voice calling

--- lib/signalling.dart
+++ lib/signalling.dart
@@ -12,11 +12,6 @@ class Signalling {
         'urls': [
           'stun:stun1.l.google.com:19302',
           'stun:stun2.l.google.com:19302',
-          {
-            "url": 'turn:numb.viagenie.ca',
-            "credential": 'muazkh',
-            "username": 'webrtc@live.com'
-          },
         ]
       }
     ]
